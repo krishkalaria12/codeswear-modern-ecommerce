1d082ccd24df9037a60e9b50607c0936e362e1e8
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-01-21 12:13:48 +0530

    removed mongodb and adding appwrite

--- app/api/pincode/route.js
+++ app/api/pincode/route.js
@@ -3,3 +3,17 @@ import { NextResponse, NextRequest } from "next/server";
 export async function GET() {
     return new NextResponse(JSON.stringify([111222,334455]), {status: 200})
 }
+
+
+// export const getProducts = async (req, res) => {
+//     try {
+//         const products = await product.find();
+
+//         return new NextResponse(JSON.stringify(products), { status: 200 });
+//     } catch (error) {
+//         console.error("Error fetching products:", error);
+//         return new NextResponse("Internal Server Error", { status: 500 });
+//     }
+// };
+
+// export default connectDb(getProducts);
